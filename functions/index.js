eafb5839c91b64ada66c90d30e27805762f6bb19
codediodeio <hello@jeffdelaney.me>
2019-01-17 17:08:58 -0700

    simplified process

--- functions/index.js
+++
@@ -1,8 +0,0 @@
-const functions = require('firebase-functions');
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.flameThrower = functions.https.onRequest((request, response) => {
- response.send("🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥");
-});
